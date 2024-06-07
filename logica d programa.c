5cc04220e36e77b71176f0a0dcd817d82d3f1ec7
eduard0estevam <e.estevamcarvalho@outlook.com>
2024-06-07 13:14:03 -0300

    Update logica d programa.c

--- logica d programa.c
+++ logica d programa.c
@@ -1,24 +1,76 @@
 #include <stdio.h>
-//espaço de 3 linhas para as outras bibliotecas.
-
-
-void main(){
-
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+int main(){
+//funçao que inicia a semente da randomização
+   srand(time(NULL));
 // atribui as variaves que eu acredito que serão necessarias
-int cont = 0 ;
-int num[10] = {0,1,2,3,4,5,6,7,8,9};
-int resultado[20];
-char inicio_jog[7] = "inicio";
-
-//primeiro laço que eu acredito ser o laço q vai ficar como geral
-do{
- printf("%s\n",inicio_jog);
- printf("quer jogar ou nao\n");
- scanf("%d",&cont);
- if(cont==0){
-    break;
- }
- }while(1);
-
-
+   int num[20];
+   int respota[10];
+   int resposta_certa[10];
+   char teste_jogo[7]="";
+   char operacao;
+   
+//funçao randomica para ser selecionado 10 numeros inteiros aleatorios
+   for(int i = 0; i<4;i++){
+      num[i]= rand % 101;
+   }
+// laço para realizar as equacoes do primeiro nivel 
+ for(int i = 0; i<4;i++){
+   operacao = operacao[rand()%4];
+   switch (operacao)
+   {
+   case '+':
+      resposta_certa[i] = num[i] + num[i+1];
+      break;
+   
+   case ''
+   default:
+      break;
+   }
+   }
+   //primeiro laço que eu acredito ser o laço q vai ficar como laço principal
+while(1){
+}
+ return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+int main(){
+//funçao que inicia a semente da randomização
+   srand(time(NULL));
+// atribui as variaves que eu acredito que serão necessarias
+   int num[20];
+   int respota[10];
+   int resposta_certa[10];
+   char teste_jogo[7]="";
+   char operacao;
+   
+//funçao randomica para ser selecionado 10 numeros inteiros aleatorios
+   for(int i = 0; i<4;i++){
+      num[i]= rand % 101;
+   }
+// laço para realizar as equacoes do primeiro nivel 
+ for(int i = 0; i<4;i++){
+   operacao = operacao[rand()%4];
+   switch (operacao)
+   {
+   case '+':
+      resposta_certa[i] = num[i] + num[i+1];
+      break;
+   
+   case ''
+   default:
+      break;
+   }
+   }
+   //primeiro laço que eu acredito ser o laço q vai ficar como laço principal
+while(1){
+}
+ return 0;
 }
