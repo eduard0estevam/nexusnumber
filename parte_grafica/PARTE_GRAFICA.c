c8cb1f05a30af910d1a7e6de88890785a85409ff
Ana Kelry <anakelry346@gmail.com>
2024-06-08 03:18:41 -0300

    Update PARTE_GRAFICA.c

---
+++ parte_grafica/PARTE_GRAFICA.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+
+void exibirMenu(int opcao) {
+    char *opcoes[] = {"Opção 1", "Opção 2", "Opção 3", "Sair"};
+    int numOpcoes = sizeof(opcoes) / sizeof(char *);
+
+    printf("\n\nMenu:\n");
+    for (int i = 0; i < numOpcoes; i++) {
+        if (i == opcao) {
+            textcolor(RED);
+            cprintf("%s\n", opcoes[i]);
+            textcolor(WHITE);
+        } else {
+            printf("%s\n", opcoes[i]);
+        }
+    }
+}
+
+int main() {
+    int opcao = 0;
+    int tecla;
+
+    do {
+        clrscr();
+        exibirMenu(opcao);
+
+        tecla = getch();
+        if (tecla == 72) { // Tecla para cima
+            if (opcao > 0) opcao--;
+        } else if (tecla == 80) { // Tecla para baixo
+            if (opcao < 3) opcao++;
+        }
+    } while (tecla != 13); // Tecla Enter para selecionar a opção
+
+    printf("\nVocê selecionou a opção %d\n", opcao + 1);
+
+    return 0;
+}
