1202675dcdc551c47212bbbb67b8babbf560a1a5
Ana Kelry <anakelry346@gmail.com>
2024-06-22 22:25:58 -0300

    logica principal do jogo

--- logica_principal_jogo.c
+++ logica_principal_jogo.c
@@ -1,61 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 #include <time.h>
 #include <windows.h>
 #include <conio.h>
 
-int main(){
+// Variáveis globais
+int vidas = 3;
+int num[20];
+int resposta[10];
+int resposta_certa[10];
+char op;
+int operacao;
+int tentativas = 1;
+int dificuldade_jogo;
+int i;
 
-    char escolhaMenu; 
+// Prototypes
+void limparTela();
+void esperar(int segundos);
+char exibirMenuPrincipal();
+char exibirMenuNivel();
+void executarJogo(char nivelDoJogo);
+void nivelfacil(int *vidas);
+void nivelmedio(int *vidas);
+void niveldificil(int *vidas);
+
+int main() {
+    char escolhaMenu;
     char nivelDoJogo;
-    
 
-        printf("\n\n\n\n                                                      Nexus Number    \n\n\n\n\n\n\n");
-        printf("                                                   1 - Iniciar Jogo    \n\n");
-        printf("                                                       2 - Sair\n");
-        
-        
+    srand(time(NULL));
 
-    while(1){
+    while (1) {
+        escolhaMenu = exibirMenuPrincipal();
 
-        escolhaMenu = getch(); //Lê um caractére sem aparecer na tela e sem precisar dar enter.
-        
-        if(escolhaMenu == '1'){
-             system("cls"); // Limpa a tela antes de imprimir a próxima frase.
-             printf("\n\n\n\n                                                  Bem-vindo ao Nexus Number!\n\n");
-
-             Sleep(2000); // Espera 2 segundos antes de limpar e imprimir o próximo menu.
-
-             system("cls");
-             printf("\n\n\n\n                                                Escolha o nivel do jogo: \n\n\n");
-             printf("                                                       1 - Facil\n\n");
-             printf("                                                       2 - Medio\n\n");
-             printf("                                                       3 - Dificil\n\n");
-             nivelDoJogo = getch();
-            
-            if(nivelDoJogo == '1'){
-                printf("Teste.\n");
-            }else if(nivelDoJogo == '2'){
-                printf("Teste 2.\n");
-            }else if(nivelDoJogo == '3'){
-                printf("Teste 3.\n");
-            }else {
-                //Nada acontece.
-            }
-
-             
-
-        } else if(escolhaMenu == '2'){
+        if (escolhaMenu == '1') {
+            limparTela();
+            printf("\n\n\n\n                                                  Bem-vindo ao Nexus Number!\n\n");
+            esperar(2);
+            limparTela();
+            nivelDoJogo = exibirMenuNivel();
+            executarJogo(nivelDoJogo);
+        } else if (escolhaMenu == '2') {
             exit(0);
-        
-        } else {
-           //Nada acontece caso a escolha não seja 1 ou 2.          
         }
     }
 
-       
-    
     return 0;
+}
+
+void limparTela() {
+    system("cls");
+}
+
+void esperar(int segundos) {
+    Sleep(segundos * 1000);
+}
+
+char exibirMenuPrincipal() {
+    printf("\n\n\n\n                                                      Nexus Number    \n\n\n\n\n\n\n");
+    printf("                                                   1 - Iniciar Jogo    \n\n");
+    printf("                                                       2 - Sair\n");
+
+    return getch();
+}
+
+char exibirMenuNivel() {
+    printf("\n\n\n\n                                                Escolha o nivel do jogo: \n\n\n");
+    printf("                                                       1 - Facil\n\n");
+    printf("                                                       2 - Medio\n\n");
+    printf("                                                       3 - Dificil\n\n");
+
+    return getch();
+}
+
+void executarJogo(char nivelDoJogo) {
+    switch (nivelDoJogo) {
+        case '1':
+            nivelfacil(&vidas);
+            break;
+        case '2':
+            nivelmedio(&vidas);
+            break;
+        case '3':
+            niveldificil(&vidas);
+            break;
+        default:
+            // Nada acontece se o nível não for 1, 2 ou 3.
+            break;
+    }
+}
+
+void nivelfacil(int *vidas) {
+    // Implementação do nível fácil
+    printf("Nível Fácil - Implementação Pendente\n");
+    // Exemplo de lógica básica
+    for(i = 0; i<4;i++){
+        num[i] = (rand () % 50) + 1;
+    }
+    // Adicione a lógica do jogo aqui
+}
+
+void nivelmedio(int *vidas) {
+    // Implementação do nível médio
+    printf("Nível Médio - Implementação Pendente\n");
+    // Exemplo de lógica básica
+    for(i = 0; i<6;i++){
+        num[i] = (rand () % 100) + 1;
+    }
+    // Adicione a lógica do jogo aqui
+}
+
+void niveldificil(int *vidas) {
+    // Implementação do nível difícil
+    printf("Nível Difícil - Implementação Pendente\n");
+    // Exemplo de lógica básica
+    for(i = 0; i<8;i++){
+        num[i] = (rand () % 200) + 1;
+    }
+    // Adicione a lógica do jogo aqui
 }
--- teste_logicadeprogamacao.c
+++ teste_logicadeprogamacao.c
@@ -1,44 +1,216 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 #include <time.h>
-#include <windows.h>
+#include <string.h>
 
-void nivelfacil(int *vidas) {
-    
-    int num[4];
-    int resposta_certa[4];
-    char operacoes[5] = {'+', '-', '*', '/'};
-    char op;
+// Estrutura para armazenar perguntas e respostas
+typedef struct {
+    char pergunta[256];
+    char resposta[256];
+} Questao;
 
-    // Geração de números aleatórios
-    for(int i = 0; i < 4; i++){
-        num[i] = rand() % 101;
+// Função para trocar duas questões
+void trocar(Questao *a, Questao *b) {
+    Questao temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Função para embaralhar as questões
+void embaralhar(Questao questoes[], int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        trocar(&questoes[i], &questoes[j]);
     }
+}
 
-    // Realização das operações
-    for(int i = 0; i < 3; i++) {
-        if(operacoes[i] == '+') {
-            resposta_certa[i] = num[i] + num[i+1];
-            op = operacoes[i];
-        } else if(operacoes[i] == '-') {
-            resposta_certa[i] = num[i] - num[i+1];
-            op = operacoes[i];
-        } else if(operacoes[i] == '*') {
-            resposta_certa[i] = num[i] * num[i+1];
-            op = operacoes[i];
-        } else if(operacoes[i] == '/') { 
-            resposta_certa[i] = num[i] / num[i+1];
-            op = operacoes[i];
+// Função para imprimir a matriz 4x4
+void imprimirMatriz(int matriz[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (matriz[i][j] == -1) {
+                printf("? ");
+            } else {
+                printf("%d ", matriz[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+// Função para validar a resposta da matriz
+int validarMatriz(int matriz[4][4], int resposta[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (matriz[i][j] != -1 && matriz[i][j] != resposta[i][j]) {
+                return 0; // Resposta errada
+            }
         }
     }
-    
-    }
+    return 1; // Resposta correta
+}
 
-nint main () {
-    int vidas = 3;
-    nivelfacil(&vidas);
-    printf("O jogo de nível fácil foi concluído.\n");
+// Função para limpar a tela
+void limparTela() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+// Função do nível fácil
+void nivelFacil() {
+   Questao questoes[5] = {
+        {"Pergunta 1: 4, 8 e 16: Qual eh o proximo numero?", "32"}, //perguntas e respostas
+        {"Pergunta 2: 6 = 30\n 3 = 15\n 7 = 35\n 2 = ?\n", "10"},
+        {"Pergunta 3: A + B = 60\nA - B = 40\n A / B = ?", "5"},
+        {"Pergunta 4: 13,18 = 31\n7,25 = 32\n12, 30 = 42\n26, 13 = ?\n", "39"},
+        {"Pergunta 5: Complete a matriz 4x4:\n"
+         "Linha 0: 2, 1, 0, 0\n"
+         "Linha 1: 4, 1, 1, 1\n"
+         "Linha 2: 6, 1, 0, 2\n"
+         "Linha 3: ?, ?, ?, ?\n"
+         "Coluna 0: 2, 4, 6, ?\n"
+         "Coluna 1: 1, 1, 1, ?\n"
+         "Coluna 2: 0, 1, 0, ?\n"
+         "Coluna 3: 0, 1, 2, ?\n", "8, 1, 1, 3"},
+    };
+
+    srand(time(NULL));
+    embaralhar(questoes, 5);
+
+    for (int i = 0; i < 5; i++) {
+        int correta = 0;
+
+        while (!correta) {
+            limparTela();
+            if (i == 4) {
+                int matriz[4][4] = {
+                    {2, 1, 0, 0},
+                    {4, 1, 1, 1},
+                    {6, 1, 0, 2},
+                    {-1, -1, -1, -1}
+                };
+
+                int resposta[4][4] = {
+                    {2, 1, 0, 0},
+                    {4, 1, 1, 1},
+                    {6, 1, 0, 2},
+                    {0, 0, 0, 0}
+                };
+
+                printf("%s\n", questoes[i].pergunta);
+                imprimirMatriz(matriz);
+
+                printf("Preencha a linha 3 (separados por espaço): ");
+                for (int j = 0; j < 4; j++) {
+                    scanf("%d", &resposta[3][j]);
+                }
+
+                if (validarMatriz((int[4][4]){{2, 1, 0, 0}, {4, 1, 1, 1}, {6, 1, 0, 2}, {8, 1, 1, 3}}, resposta)) {
+                    printf("Correto!\n");
+                    correta = 1;
+                } else {
+                    printf("Errado! Tente novamente.\n");
+                    getchar();
+                    getchar();
+                }
+            } else {
+                char resposta[256];
+                printf("%s\n", questoes[i].pergunta);
+                printf("Sua resposta: ");
+                scanf("%s", resposta);
+
+                if (strcmp(resposta, questoes[i].resposta) == 0) {
+                    printf("Correto!\n");
+                    correta = 1;
+                } else {
+                    printf("Errado! Tente novamente.\n");
+                    getchar();
+                    getchar();
+                }
+            }
+            if (correta) {
+                printf("Pressione Enter para continuar...");
+                getchar();
+                getchar();
+            }
+        }
+    }
+}
+
+// Função para exibir o submenu de dificuldade
+void selecionarDificuldade() {
+    int escolha;
+    int valido = 0;
+
+    while (!valido) {
+        limparTela();
+        printf("Selecione a dificuldade do jogo:\n");
+        printf("1. Facil\n");
+        printf("2. Medio\n");
+        printf("3. Dificil\n");
+        printf("Escolha uma opcao (1-3): ");
+        scanf("%d", &escolha);
+
+        switch (escolha) {
+            case 1:
+                nivelFacil();
+                valido = 1;
+                break;
+            case 2:
+                //printf("Nível médio ainda não implementado.\n");
+                //valido = 1;
+                break;
+            case 3:
+                //printf("Nível difícil ainda não implementado.\n");
+                valido = 1;
+                break;
+            default:
+                //printf("Opção inválida! Tente novamente.\n");
+              //getchar();
+               // getchar();
+                break;
+        }
+    }
+}
+
+// Função para exibir o menu principal
+void mostrarMenu() {
+    int escolha;
+    int valido = 0;
+
+    while (!valido) {
+        limparTela();
+        printf("Bem-vindo ao NexusNumbers!\n");
+        printf("1. Iniciar Jogo\n");
+        printf("2. Sair\n");
+        printf("Escolha uma opcao (1-2): ");
+        scanf("%d", &escolha);
+
+        switch (escolha) {
+            case 1:
+                selecionarDificuldade();
+                valido = 1;
+                break;
+            case 2:
+                printf("Saindo...\n");
+                exit(0);
+                break;
+            default:
+                printf("Opcao invalida! Tente novamente.\n");
+                getchar();
+                getchar();
+                break;
+        }
+    }
+}
+
+int main() {
+    mostrarMenu();
     return 0;
 }
+
+
+
