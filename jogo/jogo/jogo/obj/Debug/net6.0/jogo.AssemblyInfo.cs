329252d06a15c3c8cd6173f9e34cdbde72bcf0d9
Ana Kelry <anakelry346@gmail.com>
2024-06-22 21:15:37 -0300

    atualização/jogo

--- jogo/jogo/jogo/obj/Debug/net6.0/jogo.AssemblyInfo.cs
+++ jogo/jogo/jogo/obj/Debug/net6.0/jogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62cac53b33059349930293d658478a15abe127ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b78cb8db4b3d0aa40168329af0bb06ac940aece3")]
 [assembly: System.Reflection.AssemblyProductAttribute("jogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
