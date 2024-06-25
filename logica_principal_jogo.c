45385c700bdce23c2bdf33be2aed085d67105103
Ana Kelry <anakelry346@gmail.com>
2024-06-24 21:10:02 -0300

    logica

--- logica_principal_jogo.c
+++ logica_principal_jogo.c
@@ -185,11 +185,10 @@ void nivelfacil(int *vidas) {
 void nivelmedio(int *vidas) {
     Questao questoes[] = {
         {"Pergunta 1:\nEm um saco há 5 bolas vermelhas, 4 bolas azuis e 3 bolas verdes. Qual a probabilidade de se retirar uma bola azul?", "1/3"},
-        {"Pergunta 3:\nSe o dobro de um número é 24, qual é metade desse número?", " 6"},
-        {"Pergunta 5:\nSe 5 maquinas podem completar um trabalho em 8 horas, quantas horas levarao 8 maquinas para completar o mesmo trabalho?", "5"},
-        {"Pergunta 6:\nUm produto custa R$ 120,00. Após um desconto de 25%%, qual é o novo preço?", "90"},
-        {"Pergunta 7:\nSe a=1, b=2, c=3, ..., z=26, qual é a soma das letras da palavra "CAT?"", "24"},
-        {"Pergunta 8:\nQual é a soma de 1/3 e 1/4?", "7/12"},
+        {"Pergunta 2:\nSe o dobro de um número é 24, qual é metade desse número?", " 6"},
+        {"Pergunta 3:\nSe 5 maquinas podem completar um trabalho em 8 horas, quantas horas levarao 8 maquinas para completar o mesmo trabalho?", "5"},
+        {"Pergunta 4:\nUm produto custa R$ 120,00. Após um desconto de 25%%, qual é o novo preço?", "90"},
+        {"Pergunta 5:\nQual é a soma de 1/3 e 1/4?", "7/12"},
     };
 
     nivelPerguntas(vidas, questoes, 5);
@@ -197,13 +196,15 @@ void nivelmedio(int *vidas) {
 
 void niveldificil(int *vidas) {
     Questao questoes[] = {
-        {"Pergunta 1:\nDuas tartarugas caminham em direção a um lago. A primeira percorre 30 metros por dia e chega ao lago em 16 dias. A segunda tartaruga percorre 20 metros por dia. Quando a primeira tartaruga chegou ao lago, quantos dias ela teve que esperar para a segunda tartaruga chegar?", "8"},
+        {"Pergunta 1:\nDois caracóis estão se movendo em direção a um jardim. O primeiro caracol percorre 30 metros por dia e leva 16 dias para chegar ao jardim. O segundo caracol se move a uma velocidade de 20 metros por dia. Após quantos dias o segundo caracol alcançará o jardim, considerando que o primeiro caracol já está lá?", "8"},
         {"Pergunta 2:\nEm um sistema de codificação, AB representa os algarismos do dia do nascimento de uma pessoa e CD os algarismos de seu mês de nascimento. Qual é o mês de nascimento dessa pessoa se a data for trinta de julho?", "07"},
         {"Pergunta 3:\nSe 3 gatos caçam 3 ratos em 3 minutos, em quantos minutos levarão 100 gatos para caçar 100 ratos?", "3"},
-        {"Pergunta 4:\nExemplo?", "Resposta"},
+        {"Pergunta 4:\nUm carro viaja a 60 km/h. Em Quantas horaas levará para percorrer 180 km?", "3"},
+        {"Pergunta 5:\nSe a=1, b=2, c=3, ..., z=26, qual é a soma das letras da palavra CAT?", "24"},
+
     };
 
-    nivelPerguntas(vidas, questoes, 10);
+    nivelPerguntas(vidas, questoes, 5);
 }
 
 void salvarPontuacao(int pontos) {
