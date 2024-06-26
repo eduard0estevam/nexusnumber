a387cdff75c20d63d5ad4d81a5abc5190903de22
Ana Kelry <anakelry346@gmail.com>
2024-06-26 12:41:28 -0300

    nexusnumber

--- graficos/jogo/jogo/obj/Debug/net6.0/jogo.AssemblyInfo.cs
+++ graficos/jogo/jogo/obj/Debug/net6.0/jogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2448ed9fd0678e81124fdda5d4b79d1de79f8355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a478960eec99150839b90ad09b10338aa066b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("jogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
