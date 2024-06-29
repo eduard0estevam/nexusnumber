58028ac57ebf9a502d0be26d4f86266bc7175f04
kelryana <anakelry346@gmail.com>
2024-06-29 03:25:04 -0300

    biblioteca compartilhada para a logica e a parte grafica

---
+++ logica.h
@@ -0,0 +1,29 @@
+#ifndef LOGICA_H
+#define LOGICA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// Estrutura para armazenar perguntas e respostas
+typedef struct {
+    char pergunta[256];
+    char resposta[256];
+} Questao;
+
+// Estrutura para armazenar pontuação com nome do jogador
+typedef struct {
+    char nome[50];
+    int pontuacao;
+} Pontuacao;
+
+// Funções auxiliares
+void trocar(Questao *a, Questao *b);
+void embaralhar(Questao questoes[], int n);
+void nivelPerguntas(int *vidas, Questao questoes[], int total_questoes);
+void nivelFacil(int *vidas, Questao questoes[], int total_questoes);
+void nivelMedio(int *vidas, Questao questoes[], int total_questoes);
+void nivelDificil(int *vidas, Questao questoes[], int total_questoes);
+
+#endif
